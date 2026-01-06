00a22915a6b7720faa96aa3c82ed05e2f55e94fb
GioKvz <giorgi.kovziridze.1@btu.edu.ge>
2026-01-06 17:19:55 +0400

    Creating Project

---
+++ Final/Final/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Final
+//
+//  Created by nika kovziridze on 06.01.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Final/FinalTests/FinalTests.swift
@@ -0,0 +1,17 @@
+//
+//  FinalTests.swift
+//  FinalTests
+//
+//  Created by nika kovziridze on 06.01.26.
+//
+
+import Testing
+@testable import Final
+
+struct FinalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
